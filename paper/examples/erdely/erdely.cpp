71f63bf143fd35779f7b8cc90e02a0905f7fa561
Szilard Ledan <szledan@inf.u-szeged.hu>
2018-04-01 20:50:05 +0200

    [paper] Improve erdely example, integarte it
    Signed-off-by: Szilard Ledan <szledan@inf.u-szeged.hu>

--- paper/examples/erdely/erdely.cpp
+++ paper/examples/erdely/erdely.cpp
@@ -25,7 +25,9 @@
 
 #include "gepard.h"
 #include "surfaces/gepard-png-surface.h"
+#include <cmath>
 #include <iostream>
+#include <map>
 #include <string>
 
 #define CHECK_FLAG(SFLAG, LFLAG, FROM, TO, ARGV) [&]()->int { for (int _i = FROM; _i < TO; ++_i) { if (std::string(ARGV[_i]) == SFLAG || std::string(ARGV[_i]) == LFLAG) return _i; } return 0; }()
@@ -33,11 +35,335 @@
 #define PARSE_FLAG_VALUE(SFLAG, LFLAG, DEFAULT, FROM, TO, ARGC, ARGV) [&](){ int _j = CHECK_FLAG(SFLAG, LFLAG, FROM, TO, ARGV); return (_j && (++_j) < ARGC) ? ARGV[_j] : DEFAULT; }()
 #define PARSE_FLAG(SFLAG, LFLAG, DEFAULT, ARGC, ARGV) PARSE_FLAG_VALUE(SFLAG, LFLAG, DEFAULT, 1, ARGC, ARGC, ARGV);
 
-void shapeMoon(gepard::Gepard& ctx)
-{
-}
+class SzeklerWriter {
+public:
+    SzeklerWriter(gepard::Gepard& ctx, const float fontSize = 12.0)
+        : _ctx(ctx)
+        , _fontSize(fontSize)
+    {
+        _lw = _ctx.lineWidth;
+        _leadingSize = _fs * 0.7 * 0.5;
+    }
 
-void shapeStar(gepard::Gepard& ctx)
+    SzeklerWriter& text(const std::string& str)
+    {
+        return *this;
+    }
+
+    SzeklerWriter& _()
+    {
+        begin();
+        leading(0.5);
+        moveCursor(0.5);
+        return flush();
+    }
+
+    SzeklerWriter& x()
+    {
+        begin();
+        leading(0.5);
+        _ctx.save();
+
+        _ctx.moveTo( 0.07 * _fs, 0.43 * _fs);
+        _ctx.lineTo(-0.07 * _fs, 0.57 * _fs);
+        _ctx.moveTo(-0.07 * _fs, 0.43 * _fs);
+        _ctx.lineTo( 0.07 * _fs, 0.57 * _fs);
+
+        _ctx.restore();
+        moveCursor(0.5);
+        return flush();
+    }
+
+    SzeklerWriter& a()
+    {
+        begin();
+        leading(0.2);
+        _ctx.save();
+
+        _ctx.moveTo(0.0 * _fs, 0.0 * _fs);
+        _ctx.lineTo(0.0 * _fs, 1.0 * _fs);
+        _ctx.moveTo(0.0 * _fs, 0.0 * _fs);
+        _ctx.lineTo(-0.25 * _fs, 0.25 * _fs);
+        _ctx.lineTo(0.0 * _fs, 0.5 * _fs);
+
+        _ctx.restore();
+        moveCursor(0.8);
+        return flush();
+    }
+
+    SzeklerWriter& ba()
+    {
+        begin();
+        leading(0.7);
+        _ctx.save();
+
+        _ctx.moveTo(0.25 * _fs, 0.0 * _fs);
+        _ctx.lineTo(-0.25 * _fs, 1.0 * _fs);
+        _ctx.moveTo(-0.25 * _fs, 0.0 * _fs);
+        _ctx.lineTo(0.25 * _fs, 1.0 * _fs);
+        _ctx.moveTo(-0.25 * _fs, 0.0 * _fs);
+        _ctx.lineTo(-0.3 * _fs, 0.20 * _fs);
+        _ctx.lineTo(-0.1 * _fs, 0.30 * _fs);
+
+        _ctx.restore();
+        moveCursor(0.9);
+        return flush();
+    }
+
+    SzeklerWriter& d()
+    {
+        begin();
+        leading(0.7);
+        _ctx.save();
+
+        _ctx.moveTo(0.0 * _fs, 0.0 * _fs);
+        _ctx.lineTo(0.0 * _fs, 1.0 * _fs);
+        _ctx.moveTo(0.20 * _fs, 0.3 * _fs);
+        _ctx.lineTo(-0.20 * _fs, 0.7 * _fs);
+
+        _ctx.restore();
+        moveCursor(0.7);
+        return flush();
+    }
+
+    SzeklerWriter& e()
+    {
+        begin();
+        leading(0.7);
+        _ctx.save();
+
+        _ctx.moveTo(-0.1 * _fs, 0.0);
+        _ctx.bezierCurveTo(0.15 * _fs, 0.35 * _fs, 0.15 * _fs, 0.65 * _fs, -0.1 * _fs, _fs);
+        _ctx.moveTo(0.15 * _fs, 0.08 * _fs);
+        _ctx.lineTo(-0.10 * _fs, 0.30 *  _fs);
+        _ctx.moveTo(0.15 * _fs, 0.92 * _fs);
+        _ctx.lineTo(-0.10 * _fs, 0.70 *  _fs);
+
+        _ctx.restore();
+        moveCursor(0.7);
+        return flush();
+    }
+
+    SzeklerWriter& ee()
+    {
+        begin();
+        leading(0.7);
+        _ctx.save();
+
+        _ctx.moveTo(-0.1 * _fs, 0.0);
+        _ctx.bezierCurveTo(0.15 * _fs, 0.35 * _fs, 0.15 * _fs, 0.65 * _fs, -0.1 * _fs, _fs);
+        _ctx.moveTo(0.2 * _fs, 0.08 * _fs);
+        _ctx.lineTo(0.02 * _fs, 0.20 *  _fs);
+        _ctx.moveTo(0.2 * _fs, 0.92 * _fs);
+        _ctx.lineTo(0.02 * _fs, 0.8 *  _fs);
+
+        _ctx.restore();
+        moveCursor(0.7);
+        return flush();
+    }
+
+    SzeklerWriter& eely()
+    {
+        begin();
+        leading(0.7);
+        _ctx.save();
+
+        _ctx.moveTo(0.0 * _fs, 0.0);
+        _ctx.bezierCurveTo(0.25 * _fs, 0.35 * _fs, 0.25 * _fs, 0.65 * _fs, 0.0 * _fs, _fs);
+        _ctx.moveTo(0.0 * _fs, 0.0);
+        _ctx.bezierCurveTo(-0.25 * _fs, 0.35 * _fs, -0.25 * _fs, 0.65 * _fs, 0.0 * _fs, _fs);
+        _ctx.moveTo(0.03 * _fs, 0.55 * _fs);
+        _ctx.lineTo(-0.16 * _fs, 0.69 *  _fs);
+        _ctx.moveTo(0.3 * _fs, 0.08 * _fs);
+        _ctx.lineTo(0.12 * _fs, 0.20 *  _fs);
+        _ctx.moveTo(0.3 * _fs, 0.92 * _fs);
+        _ctx.lineTo(0.12 * _fs, 0.8 *  _fs);
+
+        _ctx.restore();
+        moveCursor(0.7);
+        return flush();
+    }
+
+    SzeklerWriter& ly()
+    {
+        begin();
+        leading(0.7);
+        _ctx.save();
+
+        _ctx.moveTo(0.0 * _fs, 0.0);
+        _ctx.bezierCurveTo(0.25 * _fs, 0.35 * _fs, 0.25 * _fs, 0.65 * _fs, 0.0 * _fs, _fs);
+        _ctx.moveTo(0.0 * _fs, 0.0);
+        _ctx.bezierCurveTo(-0.25 * _fs, 0.35 * _fs, -0.25 * _fs, 0.65 * _fs, 0.0 * _fs, _fs);
+        _ctx.moveTo(0.03 * _fs, 0.55 * _fs);
+        _ctx.lineTo(-0.16 * _fs, 0.69 *  _fs);
+
+        _ctx.restore();
+        moveCursor(0.7);
+        return flush();
+    }
+
+    SzeklerWriter& R()
+    {
+        begin();
+        leading(0.9);
+        _ctx.save();
+
+        _ctx.moveTo(0.2 * _fs, 0.0 * _fs);
+        _ctx.lineTo(0.2 * _fs, 1.0 * _fs);
+        _ctx.moveTo(-0.2 * _fs, 0.0 * _fs);
+        _ctx.lineTo(-0.2 * _fs, 1.0 * _fs);
+        _ctx.moveTo(0.2 * _fs, 0.40 * _fs);
+        _ctx.lineTo(-0.2 * _fs, 0.65 *  _fs);
+
+        _ctx.restore();
+        moveCursor(0.9);
+        return flush();
+    }
+
+    SzeklerWriter& Rd()
+    {
+        begin();
+        leading(1.0);
+        _ctx.save();
+
+        _ctx.moveTo(0.3 * _fs, 0.0 * _fs);
+        _ctx.lineTo(0.3 * _fs, 1.0 * _fs);
+        _ctx.moveTo(-0.10 * _fs, 0.0 * _fs);
+        _ctx.lineTo(-0.10 * _fs, 1.0 * _fs);
+        _ctx.moveTo(0.3 * _fs, 0.40 * _fs);
+        _ctx.lineTo(-0.3 * _fs, 0.65 *  _fs);
+
+        _ctx.restore();
+        moveCursor(1.0);
+        return flush();
+    }
+
+    SzeklerWriter& r()
+    {
+        begin();
+        leading(0.7);
+        _ctx.save();
+
+        _ctx.moveTo(0.2 * _fs, 0.40 * _fs);
+        _ctx.lineTo(-0.2 * _fs, 0.65 *  _fs);
+
+        _ctx.restore();
+        moveCursor(0.7);
+        return flush();
+    }
+
+    SzeklerWriter& sz()
+    {
+        begin();
+        leading(0.3);
+        _ctx.save();
+
+        _ctx.moveTo(0.0 * _fs, 0.0 * _fs);
+        _ctx.lineTo(0.0 * _fs, 1.0 * _fs);
+
+        _ctx.restore();
+        moveCursor(0.5);
+        return flush();
+    }
+
+    SzeklerWriter& STAR(const float size = 0.8)
+    {
+        begin();
+        leading(1.5);
+        _ctx.save();
+
+        const float s = _fs * size;
+        const float _1p6 = s * 0.5 / 3.0;
+        const float _2p6 = s * 1.0 / 3.0;
+        const float _3p6 = s * 0.5;
+        const float kPi = 2.0 * std::asin(1.0);
+
+        _ctx.translate(0.0, 0.5 * _fs);
+        _ctx.moveTo(-_1p6, -_2p6);
+        for (int i = 0; i < 4; ++i) {
+            _ctx.lineTo(-_1p6, -_3p6);
+            _ctx.lineTo(0.0, -_2p6);
+            _ctx.lineTo(_1p6, -_3p6);
+            _ctx.lineTo(_1p6, -_1p6);
+            _ctx.rotate(kPi / 2.0);
+        }
+        _ctx.closePath();
+        _ctx.translate(0.0, -0.5 * _fs);
+
+        _ctx.restore();
+        _ctx.fill();
+        moveCursor(1.5);
+        return flush();
+    }
+
+    SzeklerWriter& MOON(const float size = 0.8)
+    {
+        begin();
+        leading(1.4);
+        _ctx.save();
+
+        const float s = _fs * size;
+
+        _ctx.translate(0.0, 0.5 * _fs);
+        _ctx.moveTo(0.5 * s, 0.0 * s);
+        _ctx.bezierCurveTo(0.5 * s, 0.5 * s,-0.05 * s, 0.6 * s,  -0.1 * s, 0.48 * s);
+        _ctx.bezierCurveTo(0.25 * s, 0.2 * s, 0.25 * s, -0.2 * s, -0.1 * s, -0.48 * s);
+        _ctx.bezierCurveTo(-0.05 * s, -0.6 * s, 0.5 * s, -0.5 * s, 0.5 * s, 0.0 * s);
+        _ctx.closePath();
+        _ctx.translate(0.0, -0.5 * _fs);
+
+        _ctx.restore();
+        _ctx.fill();
+        moveCursor(0.4);
+        return flush();
+    }
+
+    SzeklerWriter& stroke()
+    {
+        _ctx.stroke();
+        return *this;
+    }
+
+    SzeklerWriter& fill()
+    {
+        _ctx.fill();
+        return *this;
+    }
+
+private:
+    void begin()
+    {
+        _ctx.beginPath();
+    }
+
+    SzeklerWriter& flush()
+    {
+        stroke();
+        return *this;
+    }
+
+    void moveCursor(const float size = 1.0)
+    {
+        _ctx.translate(size * _direction * _leadingSize, 0.0);
+    }
+
+    void leading(const float size = 1.0)
+    {
+        moveCursor(size);
+    }
+
+    const float _direction = -1.0;
+
+    gepard::Gepard& _ctx;
+    float _fontSize;
+    float _leadingSize;
+
+    const float& _fs = _fontSize;
+    const float& _ls = _leadingSize;
+    float _lw;
+};
+
+void shapeMoon(gepard::Gepard& ctx)
 {
 }
 
@@ -51,7 +377,7 @@ int main(int argc, char* argv[])
     }
 
     const uint width = 2048;
-    const uint height = 768;
+    const uint height = 512;
     gepard::PNGSurface surface(width, height);
     gepard::Gepard ctx(&surface);
 
@@ -61,14 +387,15 @@ int main(int argc, char* argv[])
 
     ctx.fillStyle = "#b4e256";
     ctx.strokeStyle = "#25385e";
-    ctx.lineWidth = 30;
+    ctx.lineWidth = 17.0;
     ctx.lineCap = "round";
     ctx.lineJoin = "miter";
 
-    shapeMoon(ctx);
-    shapeStar(ctx);
+    ctx.translate(1800, 120);
+    SzeklerWriter szw(ctx, 250);
+    szw.MOON()._().e().Rd().eely().x().sz().a().ba().d()._().STAR();
 
-    std::string pngFile = argc > 1 ? argv[1] : "./paper/src/img/triangle.png";
+    std::string pngFile = argc > 1 ? argv[1] : "./paper/src/img/erdely.png";
     surface.save(pngFile);
 
     return 0;
